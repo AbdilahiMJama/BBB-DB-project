f45898de1f2af959f9ac472424ae8abb0b55c8bf
Eli Johnson <eli.timothy@gmail.com>
2025-02-21 10:57:02 -0600

    added setup.py

---
+++ test/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 21 10:25:37 2025
+
+@author: Eli Johnson
+"""
+
+
+import connect_iabb as ci
+import pandas as pd
+import os
+
+
+###################################
+# Global variables and structures #
+###################################
+
+SCRIPT_NAME = 'data_quality_s2025'
+SCRIPT_VERSION = '1.0'
+CONNECT_USER = 'ABDI'
+CONNECT_DB = 'MNSU'
+CONNECT_INSTANCE = 'SANDBOX'
+
+
+
